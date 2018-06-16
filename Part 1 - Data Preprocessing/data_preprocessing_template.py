ee29662e0951d8447a4899129a3bb4739c082f51
RmViana <raj.alves96@gmail.com>
2018-06-15 22:52:53 -0300

    Import of dataset and setting of dependent and independent variables

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -8,3 +8,8 @@ Created on Fri Jun 15 21:35:05 2018
 import numpy as np
 import matplotlib as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, 3].values
