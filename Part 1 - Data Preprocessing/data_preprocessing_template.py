4529a222f5b7d9cf6516569da6f2d61d336d39dd
Romildo Alves <raj.alves96@gmail.com>
2018-07-13 20:06:30 -0300

    Taking care of missing data in R

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -11,5 +11,10 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:,:-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:, 3].values
+
+# Taking care of the missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'Nan', strategy = 'mean', axis = 0)
+imputer.fit(X)
