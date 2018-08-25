16f3ed1caab98360f323fab896a6bf0c3a62a62d
Romildo Alves <raj.alves96@gmail.com>
2018-08-25 11:08:30 -0300

    Plotting the results charts on the screen

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -2,7 +2,7 @@
 
 #Importing the libraries
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
@@ -27,3 +27,19 @@ regressor.fit(X_train, Y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+#Visualising  the Training set results
+plt.scatter(X_train, Y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#Visualising the Test set results
+plt.scatter(X_test, Y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
