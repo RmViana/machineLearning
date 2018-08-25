f449ed00b6d700bfd3049517aae197caa3772ca6
Romildo Alves <raj.alves96@gmail.com>
2018-08-25 10:41:10 -0300

    Creating a simple linear regressor, and fitting it to the training sets

--- Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -19,3 +19,8 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 1/3, rando
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test= sc_X.transform(X_test)"""
+
+# Fitting Simple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
